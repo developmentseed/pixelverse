39f7b814f93967391d6b4f1a8a1ab12363315b20
Wei Ji <23487320+weiji14@users.noreply.github.com>
2026-01-13 10:14:47 +1300

    Type-hint fix to get_weights and remove type: ignore bypasses (#23)
    * style: fix type-hint for get_weights output as WeightsEnum
    
    Replace output type of `get_weights` from `Weights` to `WeightsEnum`, and also remove the `# type: ignore` bypass. Also refactored `create_model` function slightly to call `get_weights` in place of `_model_weights[name]` twice, and added docstrings too.
    
    * style: remove # type: ignore statements
    
    Fix `invalid-argument-type` for bbox parameter, and remove unneeded `unresolved-attribute` bypasses.
    
    * Add pandas-stubs
    
    Public type stubs for pandas!

--- src/pixelverse/models/registry.py
+++ src/pixelverse/models/registry.py
@@ -1,7 +1,8 @@
+from collections.abc import Callable
 from typing import Any
 
 import torch
-from torchvision.models._api import Weights
+from torchvision.models._api import WeightsEnum
 
 from pixelverse.models.tessera import (
     TESSERA_WEIGHTS,
@@ -10,13 +11,13 @@ from pixelverse.models.tessera import (
     tessera_s2_encoder,
 )
 
-_models = {
+_models: dict[str, Callable[..., torch.nn.Module]] = {
     "tessera": tessera,
     "tessera_s2_encoder": tessera_s2_encoder,
     "tessera_s1_encoder": tessera_s1_encoder,
 }
 
-_model_weights = {
+_model_weights: dict[str, WeightsEnum] = {
     "tessera": TESSERA_WEIGHTS.TESSERA,
     "tessera_s2_encoder": TESSERA_WEIGHTS.TESSERA_S2_ENCODER,
     "tessera_s1_encoder": TESSERA_WEIGHTS.TESSERA_S1_ENCODER,
@@ -27,18 +28,42 @@ def list_models() -> list[str]:
     return list(_models.keys())
 
 
-def get_weights(name: str) -> Weights:
+def get_weights(name: str) -> WeightsEnum:
     if name not in _model_weights:
         raise KeyError(f"Model {name} not found")
-    return _model_weights[name]  # type: ignore
+    return _model_weights[name]
 
 
 def create_model(
     name: str, pretrained: bool = True, *args: Any, **kwargs: Any
-) -> tuple[torch.nn.Module, torch.nn.Sequential]:
+) -> tuple[torch.nn.Module, Callable]:
+    """
+    Load neural network model and transforms.
+
+    Parameters
+    ----------
+    name : str
+        Name of model to instantiate.
+    pretrained : bool
+        Load pretrained model weights. Default: True
+    *args
+        Additional arguments passed to model creation function.
+    **kwargs
+        Extra keyword arguments to model function: refer to each model's documentation
+        for a list of all possible arguments.
+
+    Returns
+    -------
+    model : torch.nn.Module
+        Loaded neural network model.
+    transforms : Callable
+        Transforms to apply on data before passing passing into model.
+
+    """
     if pretrained:
-        model = _models[name](_model_weights[name], *args, **kwargs)
-        transforms = _model_weights[name].transforms
+        weights = get_weights(name=name)
+        model = _models[name](weights, *args, **kwargs)
+        transforms: Callable = weights.transforms
     else:
         model = _models[name](*args, **kwargs)
         transforms = torch.nn.Sequential(torch.nn.Identity())
